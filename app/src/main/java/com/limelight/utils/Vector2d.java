3d9b75d6441d2042b73df7791bd8333dc830fe59
Meven Courouble <meven.courouble@vouc.me>
2026-03-07 13:02:05 +0100

    Vector2d: Remove dead ZERO constant and getNormalized()
    - ZERO is a mutable shared instance: any caller passing it to initialize()
      or scalarMultiply() corrupt it for others + it's unused
    - getNormalized() is broken because there's no division by zero handling
      + it's unused

--- app/src/main/java/com/limelight/utils/Vector2d.java
+++ app/src/main/java/com/limelight/utils/Vector2d.java
@@ -4,9 +4,7 @@ public class Vector2d {
     private float x;
     private float y;
     private double magnitude;
-    
-    public static final Vector2d ZERO = new Vector2d();
-    
+
     public Vector2d() {
         initialize(0, 0);
     }
@@ -20,11 +18,7 @@ public class Vector2d {
     public double getMagnitude() {
         return magnitude;
     }
-    
-    public void getNormalized(Vector2d vector) {
-        vector.initialize((float)(x / magnitude), (float)(y / magnitude));
-    }
-    
+
     public void scalarMultiply(double factor) {
         x = (float)(x * factor);
         y = (float)(y * factor);
